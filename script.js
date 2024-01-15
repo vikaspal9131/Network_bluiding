37ddb243c243d6607a6dc7fa1a42ff21dcf11539
vikas_pal <vikaspal968562@gmail.com>
2024-01-15 10:33:49 +0530

    add the copy rights section

--- script.js
+++ script.js
@@ -22,53 +22,6 @@ ScrollTrigger.scrollerProxy("#main", {
 });
 
 
-// --- RED PANEL ---
-gsap.from(".line-1", {
-  scrollTrigger: {
-    trigger: ".line-1",
-    scroller: ".smooth-scroll",
-    scrub: true,
-    start: "top bottom",
-    end: "top top",
-    onUpdate: self => console.log(self.direction)
-  },
-  scaleX: 0,
-  transformOrigin: "left center", 
-  ease: "none"
-});
-
-
-// --- ORANGE PANEL ---
-gsap.from(".line-2", {
-  scrollTrigger: {
-    trigger: ".orange",
-    scroller: ".smooth-scroll",
-    scrub: true,
-    pin: true,
-    start: "top top",
-    end: "+=100%"
-  },
-  scaleX: 0, 
-  transformOrigin: "left center", 
-  ease: "none"
-});
-
-
-// --- PURPLE/GREEN PANEL ---
-var tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".purple",
-      scroller: ".smooth-scroll",
-      scrub: true,
-      pin: true,
-      start: "top top",
-      end: "+=100%"
-    }
-  });
-
-tl.from(".purple p", {scale: 0.3, rotation:45, autoAlpha: 0, ease: "power2"})
-  .from(".line-3", {scaleX: 0, transformOrigin: "left center", ease: "none"}, 0)
-  .to(".purple", {backgroundColor: "#28a92b"}, 0);
 
 
 
